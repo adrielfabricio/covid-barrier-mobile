f1bbd5b9771c15448fa7865cc5640316565f2711
Adriel Fabrício <adrielfabricios@gmail.com>
2020-08-07 20:21:53 -0300

    :wolf: Create login screen

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button } from 'react-native-elements';
 
-// import { Container } from './styles';
+import { Container, Logo, Input } from './styles';
 
 const Login = () => {
-  return <View />;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <Container>
+      <Logo source={require('../../assets/logo.png')} resizeMode="contain" />
+      <Input
+        placeholder="E-mail"
+        value={email}
+        onChangeText={(value) => setEmail(value)}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Input
+        placeholder="Senha"
+        value={password}
+        onChangeText={(value) => setPassword(value)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      <Button title="ENTRAR" containerStyle={{ width: '90%' }} />
+    </Container>
+  );
 };
 
 export default Login;
