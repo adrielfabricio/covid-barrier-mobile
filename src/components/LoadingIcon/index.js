7f3385e0cf8790dc915193d00578cf9c4ffbe87a
SerraZ3 <serra.henrique3@gmail.com>
2020-08-08 15:28:27 -0300

    :wolf: Created LoadingIcon

---
+++ src/components/LoadingIcon/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+
+const LoadingIcon = () => {
+  return <ActivityIndicator size="large" color={'#ed1b23ff'} />;
+};
+
+export default LoadingIcon;
