237c5b23ed8c6c790f5a8d5285d125dc140e3459
SerraZ3 <serra.henrique3@gmail.com>
2020-08-08 17:39:50 -0300

    :wolf: Created model for initial views

---
+++ src/views/BarrierAdd/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const BarrierAdd = () => {
+  return <View />;
+};
+
+export default BarrierAdd;
