f72bb1dd0c41a87b2aafbc5a96113b5c97008ad8
SerraZ3 <serra.henrique3@gmail.com>
2020-08-08 15:28:13 -0300

    :wolf: Added userRefresh and position relative

--- src/components/ButtonBack/index.js
+++ src/components/ButtonBack/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Button, Icon } from 'react-native-elements';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 import { Container, ButtonContainerStyle } from './styles';
-const ButtonBack = ({ navigation, bottom }) => {
+import { useDispatch } from 'react-redux';
+
+const ButtonBack = ({ bottom, position, userRefresh }) => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const setRefreshUser = () => {
+    dispatch({ type: 'LIST_USER_REFRESH' });
+  };
   return (
     <Container
       style={[
         bottom ? { bottom: 20 } : { top: 30 },
         { alignSelf: 'flex-start' },
+        position ? { position: 'relative' } : null,
       ]}>
       <Button
         buttonStyle={{ backgroundColor: 'white' }}
-        onPress={() => navigation.goBack()}
+        onPress={() => {
+          navigation.goBack();
+          if (userRefresh) setRefreshUser();
+        }}
         icon={
           <Icon name="arrow-left" type="font-awesome" size={11} color="red" />
         }
