e6e5661d280ab115a9fd512adaf47e54a1dfa2b0
Adriel Fabrício <adrielfabricios@gmail.com>
2020-08-07 23:55:48 -0300

    :wolf: created login auth

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,12 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { Text, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { Text, TouchableHighlight } from 'react-native';
 import { Button, Divider } from 'react-native-elements';
+import { useDispatch } from 'react-redux';
 
+import login from '../../services/login';
 import { Container, Logo, Input, Footer } from './styles';
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const Login = ({ navigation }) => {
+  const [submit, setSubmit] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('serra.henrique1@gmail.com');
+  const [password, setPassword] = useState('henrique123');
+  const dispatch = useDispatch();
+
+  const setAuth = (refresh_token, token, type) => {
+    dispatch({ type: 'ADD_AUTH', auth: { refresh_token, token, type } });
+  };
+
+  useEffect(() => {
+    const handleSubmit = async () => {
+      try {
+        setLoading(true);
+
+        let { data: response } = await login(email, password);
+        setAuth(response.refreshToken, response.token, response.type);
+        console.log(response);
+
+        setSubmit(false);
+        setLoading(false);
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'AgentTab' }],
+        });
+      } catch (e) {
+        console.log(e);
+        alert('Credenciais inválidas');
+        setSubmit(false);
+        setLoading(false);
+      }
+    };
+
+    if (submit === true) handleSubmit();
+  }, [submit]);
 
   return (
     <Container>
@@ -28,6 +63,7 @@ const Login = () => {
       />
       <Button
         title="ENTRAR"
+        onPress={() => setSubmit(true)}
         containerStyle={{ width: '90%', marginBottom: '2%' }}
         buttonStyle={{ backgroundColor: '#b31b23ff' }}
       />
@@ -49,6 +85,8 @@ const Login = () => {
         buttonStyle={{
           backgroundColor: 'transparent',
         }}
+        loading={loading}
+        loadingProps={{ size: 10 }}
       />
       <Footer>
         <TouchableHighlight>
