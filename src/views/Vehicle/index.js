26f9f0d824d8bd6f1da73fa27522ed50fbca2796
Adriel Fabrício <adrielfabricios@gmail.com>
2020-08-20 15:33:38 -0300

    feat: add vehicle info into store in vehicle component

--- src/views/Vehicle/index.js
+++ src/views/Vehicle/index.js
@@ -1,18 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Text, Button } from 'react-native-elements';
 import { Picker } from '@react-native-community/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { useDispatch } from 'react-redux';
 
-import ibge from '../../services/ibge';
 import { Container, FormView, Input } from './styles';
 
-const Vehicle = () => {
+import { dateLess10 } from '../../helpers';
+import ibge from '../../services/ibge';
+
+const Vehicle = ({ navigation }) => {
   const [submit, setSubmit] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [cities, setCities] = useState([]);
-  const [license_plate, setLicensePlate] = useState('');
-  const [amount_people, setAmountPeople] = useState('');
+  const [license_plate, setLicensePlate] = useState('cffgqnw');
+  const [amount_people, setAmountPeople] = useState('2');
   const [estimed_time, setEstimedTime] = useState('');
-  const [reason_visit, setReasonVisit] = useState('');
-  const [city_origin, setCityOrigin] = useState('');
+  const [reason_visit, setReasonVisit] = useState('lorem');
+  const [city_origin, setCityOrigin] = useState('Itabuna');
+
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
+  const refDatePicker = useRef(null);
+
+  const dispatch = useDispatch();
+
+  // datepicker
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    let day = dateLess10(currentDate.getDate());
+    let month = dateLess10(currentDate.getMonth() + 1);
+    let year = currentDate.getFullYear();
+    let newDate = `${day}/${month}/${year}`;
+
+    setDate(currentDate);
+    setEstimedTime(newDate);
+    refDatePicker.current.blur();
+  };
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+  const showDatepicker = () => {
+    showMode('date');
+  };
+
+  const setVehicle = (
+    license_plate,
+    amount_people,
+    estimed_time,
+    reason_visit,
+    city_origin,
+    uf_state_origin = 'BA',
+    state_origin = 'Bahia',
+  ) => {
+    dispatch({
+      type: 'SET_VEHICLE',
+      vehicle: {
+        license_plate,
+        amount_people,
+        estimed_time,
+        reason_visit,
+        origin_visit: {
+          city_origin,
+          uf_state_origin,
+          state_origin,
+        },
+      },
+    });
+  };
 
   useEffect(() => {
     const loadCities = async () => {
@@ -26,6 +84,35 @@ const Vehicle = () => {
 
     loadCities();
   }, []);
+
+  useEffect(() => {
+    const handleSubmit = async () => {
+      try {
+        setLoading(true);
+
+        setVehicle(
+          license_plate,
+          amount_people,
+          estimed_time,
+          reason_visit,
+          city_origin,
+        );
+        setSubmit(false);
+        setLoading(false);
+
+        navigation.navigate('VehicleHistoric');
+      } catch (err) {
+        console.log(err);
+        alert('Ops! Verifique o formulário, algo não esta certo.');
+        setSubmit(false);
+        setLoading(false);
+      }
+    };
+
+    if (submit === true) {
+      handleSubmit();
+    }
+  }, [submit]);
 
   return (
     <Container>
@@ -59,14 +146,22 @@ const Vehicle = () => {
           }}
         />
         <Input
-          placeholder="Tempo estimado de permanência"
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={estimed_time}
-          onChangeText={(value) => {
-            setEstimedTime(value);
-          }}
+          placeholder={'Tempo estimado de permanência'}
+          keyboard={true}
+          value={`${estimed_time}`}
+          onFocus={showDatepicker}
+          ref={refDatePicker}
         />
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+          />
+        )}
         <Input
           placeholder="Razão da visita"
           autoCapitalize="none"
@@ -90,9 +185,10 @@ const Vehicle = () => {
 
         <Button
           title="Verificar"
-          onPress={() => console.log(cities)}
+          onPress={() => setSubmit(true)}
           containerStyle={{ width: '90%' }}
           buttonStyle={{ backgroundColor: '#b31b23ff' }}
+          loading={loading}
           loadingProps={{ size: 10 }}
         />
       </FormView>
